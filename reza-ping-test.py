f172c7e5fc5a688019928b11165779c5ab75e739
reza <yadegarireza50@googlemail.com>
2024-10-18 16:48:10 +0330

    ping test cod was changed

--- reza-ping-test.py
+++ reza-ping-test.py
@@ -1,27 +1,6 @@
-import subprocess  
-import time  
+import subprocess
 
-def ping_test(remote_server):  
-    print(f"Pinging {remote_server}...\n")  
-    try:  
-        # Execute the ping command with a timeout of 5 seconds  
-        result = subprocess.run(  
-            ["ping", "-c", "5", remote_server],  
-            capture_output=True,  
-            text=True,  
-            timeout=5  # This will stop after 5 seconds  
-        )  
-        
-        # Output the results  
-        print(result.stdout)  
-        
-        # Check for errors in the result  
-        if result.returncode != 0:  
-            print(f"Ping failed: {result.stderr}")  
-        
-    except subprocess.TimeoutExpired:  
-        print("Ping command timed out.")  
-    except Exception as e:  
-        print(f"Error executing ping: {e}")
-    finally:
-        print("this is test for Git")
+def ping_test():
+    result = subprocess.run(['ping', '-c','5','google.com'],capture_output=True, text=True)
+    print(result.stdout)
+ping_test()
